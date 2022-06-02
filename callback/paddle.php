e02857ae5a929bbfe01ba0adbdcfd608951d9c67
rtraselbd@ <rtraselbd@gmail.com>
2022-06-02 17:05:49 +0600

    First Release

---
+++ callback/paddle.php
@@ -0,0 +1,333 @@
+<?php
+
+/**
+ * Paddle WHMCS Gateway
+ *
+ * Copyright (c) 2022 UuddoktaPay
+ * Website: https://uddoktapay.com
+ * Developer: rtrasel.com
+ * 
+ */
+
+require_once __DIR__ . '/../../../init.php';
+require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
+require_once __DIR__ . '/../../../includes/invoicefunctions.php';
+
+class Paddle
+{
+    /**
+     * @var self
+     */
+    private static $instance;
+
+    /**
+     * @var string
+     */
+    protected $gatewayModuleName;
+
+    /**
+     * @var array
+     */
+    protected $gatewayParams;
+
+    /**
+     * @var boolean
+     */
+    public $isActive;
+
+    /**
+     * @var integer
+     */
+    protected $customerCurrency;
+
+    /**
+     * @var object
+     */
+    protected $gatewayCurrency;
+
+    /**
+     * @var integer
+     */
+    protected $clientCurrency;
+
+    /**
+     * @var float
+     */
+    protected $convoRate;
+
+    /**
+     * @var array
+     */
+    protected $invoice;
+
+    /**
+     * @var float
+     */
+    protected $due;
+
+    /**
+     * @var int
+     */
+    public $invoiceID;
+
+    /**
+     * @var float
+     */
+    public $total;
+
+    /**
+     * UddoktaPay constructor.
+     */
+    public function __construct()
+    {
+        $this->setGateway();
+    }
+
+    /**
+     * The instance.
+     *
+     * @return self
+     */
+    public static function init()
+    {
+        if (self::$instance == null) {
+            self::$instance = new Paddle;
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Set the payment gateway.
+     */
+    private function setGateway()
+    {
+        $this->gatewayModuleName = basename(__FILE__, '.php');
+        $this->gatewayParams     = getGatewayVariables($this->gatewayModuleName);
+        $this->isActive          = !empty($this->gatewayParams['type']);
+    }
+
+    /**
+     * Set the invoice.
+     */
+    private function setInvoice()
+    {
+        $this->invoice = localAPI('GetInvoice', [
+            'invoiceid' => $this->invoiceID
+        ]);
+
+        $this->setCurrency();
+        $this->setDue();
+        $this->setTotal();
+    }
+
+    /**
+     * Set currency.
+     */
+    private function setCurrency()
+    {
+        $this->gatewayCurrency  = (int) $this->gatewayParams['convertto'];
+        $this->customerCurrency = (int) \WHMCS\Database\Capsule::table('tblclients')
+            ->where('id', '=', $this->invoice['userid'])
+            ->value('currency');
+
+        if (!empty($this->gatewayCurrency) && ($this->customerCurrency !== $this->gatewayCurrency)) {
+            $this->convoRate = \WHMCS\Database\Capsule::table('tblcurrencies')
+                ->where('id', '=', $this->gatewayCurrency)
+                ->value('rate');
+        } else {
+            $this->convoRate = 1;
+        }
+    }
+
+    /**
+     * Set due.
+     */
+    private function setDue()
+    {
+        $this->due = $this->invoice['balance'];
+    }
+
+    /**
+     * Set total.
+     */
+    private function setTotal()
+    {
+        $this->total = ceil(($this->due) * $this->convoRate);
+    }
+
+    /**
+     * Check if transaction if exists.
+     *
+     * @param string $trxId
+     *
+     * @return mixed
+     */
+    private function checkTransaction($trxId)
+    {
+        return localAPI(
+            'GetTransactions',
+            ['transid' => $trxId]
+        );
+    }
+
+    /**
+     * Log the transaction.
+     *
+     * @param array $payload
+     *
+     * @return mixed
+     */
+    private function logTransaction($payload)
+    {
+        return logTransaction(
+            $this->gatewayParams['name'],
+            $payload,
+            $payload['status']
+        );
+    }
+
+    /**
+     * Add transaction to the invoice.
+     *
+     * @param string $trxId
+     *
+     * @return array
+     */
+    private function addTransaction($trxId)
+    {
+        $fields = [
+            'invoiceid' => $this->invoice['invoiceid'],
+            'transid'   => $trxId,
+            'gateway'   => $this->gatewayModuleName,
+            'date'      => \Carbon\Carbon::now()->toDateTimeString(),
+            'amount'    => $this->due,
+            'fees'      => 0,
+        ];
+        $add    = localAPI('AddInvoicePayment', $fields);
+
+        return array_merge($add, $fields);
+    }
+
+    /**
+     * Execute the payment by ID.
+     *
+     * @return array
+     */
+    private function executePayment()
+    {
+        $key = $this->gatewayParams['publicKey'];
+
+        if (str_contains($key, 'BEGIN PUBLIC KEY') && str_contains($key, 'END PUBLIC KEY')) {
+            $public_key = $this->gatewayParams['publicKey'];
+        } else {
+            $public_key = "-----BEGIN PUBLIC KEY-----" . "\n" . "{$key}" . "\n" . "-----END PUBLIC KEY-----";
+        }
+
+        $signature = base64_decode($_POST['p_signature']);
+
+        $fields = $_POST;
+
+        unset($fields['p_signature']);
+
+        ksort($fields);
+        foreach ($fields as $k => $v) {
+            if (!in_array(gettype($v), array('object', 'array'))) {
+                $fields[$k] = "$v";
+            }
+        }
+        $data = serialize($fields);
+
+        // Verify the signature
+        $verification = openssl_verify($data, $signature, $public_key, OPENSSL_ALGO_SHA1);
+
+        if ($verification != 1) {
+            return [
+                'status'    => 'error',
+                'message'   => 'Invalid API Signature.'
+            ];
+        }
+
+        if (is_array($fields)) {
+            return $fields;
+        }
+
+        return [
+            'status'    => 'error',
+            'message'   => 'Invalid response from UddoktaPay API.'
+        ];
+    }
+
+    /**
+     * Make the transaction.
+     *
+     * @return array
+     */
+    public function makeTransaction()
+    {
+        $executePayment = $this->executePayment();
+
+        if (!isset($executePayment['status']) && !isset($executePayment['alert_name'])) {
+            return [
+                'status'    => 'error',
+                'message'   => 'Invalid Response.',
+            ];
+        }
+
+        if (isset($executePayment['alert_name']) && $executePayment['alert_name'] === 'subscription_payment_succeeded' || $executePayment['alert_name'] === 'payment_succeeded') {
+
+            $this->invoiceID = $executePayment['passthrough'];
+            $this->setInvoice();
+
+            $existing = $this->checkTransaction($executePayment['order_id']);
+
+            if ($existing['totalresults'] > 0) {
+                return [
+                    'status'    => 'error',
+                    'message'   => 'The transaction has been already used.'
+                ];
+            }
+            if (!empty($this->gatewayParams["taxInclusive"])) {
+                $paymentAmount = $executePayment['balance_gross'];
+            } else {
+                $paymentAmount = $executePayment['balance_fee'] + $executePayment['balance_earnings'];
+            }
+
+            if ($paymentAmount < $this->total) {
+                return [
+                    'status'    => 'error',
+                    'message'   => 'You\'ve paid less than amount is required.'
+                ];
+            }
+
+            $this->logTransaction($executePayment);
+
+            $trxAddResult = $this->addTransaction($executePayment['order_id']);
+
+            if ($trxAddResult['result'] === 'success') {
+                return [
+                    'status'  => 'success',
+                    'message' => 'The payment has been successfully verified.',
+                ];
+            }
+        }
+
+        return [
+            'status'    => 'error',
+            'message'   => 'Invalid Response.',
+        ];
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    die("Direct access forbidden.");
+}
+
+$Paddle = Paddle::init();
+
+if (!$Paddle->isActive) {
+    die("The gateway is unavailable.");
+}
+
+$response = $Paddle->makeTransaction();
+die(json_encode($response));
---
+++ paddle.php
@@ -0,0 +1,160 @@
+<?php
+
+/**
+ * Paddle WHMCS Modules
+ *
+ * Copyright (c) 2022 RtRasel
+ * Website: https://rtrasel.com
+ * Developer: Md Rasel Islam
+ * Facebook: https://facebook.com/rtraselbd
+ * 
+ */
+
+if (!defined("WHMCS")) {
+    die("This file cannot be accessed directly");
+}
+
+function paddle_MetaData()
+{
+    return array(
+        'DisplayName' => 'Paddle Gateway',
+        'APIVersion' => '1.0',
+        'DisableLocalCredtCardInput' => true,
+        'TokenisedStorage' => false,
+    );
+}
+
+function paddle_config($params)
+{
+    $systemUrl = $params['systemurl'];
+    return array(
+        'FriendlyName' => array(
+            'Type' => 'System',
+            'Value' => 'Paddle Gateway',
+        ),
+        'accountID' => array(
+            'FriendlyName' => 'Paddle Vendor ID',
+            'Type' => 'text',
+            'Size' => '25',
+            'Default' => '',
+            'Description' => 'Enter your Vendor ID here (Developer Tools -> Authentication)',
+        ),
+        'secretKey' => array(
+            'FriendlyName' => 'Paddle Auth Code',
+            'Type' => 'text',
+            'Size' => '50',
+            'Default' => '',
+            'Description' => 'Enter your Paddle Auth Code here (Developer Tools -> Authentication -> Reveal Auth Code)',
+        ),
+        'publicKey' => array(
+            'FriendlyName' => 'Paddle Public Key',
+            'Type' => 'textarea',
+            'Rows' => '3',
+            'Cols' => '60',
+            'Description' => 'Enter your Paddle Auth Code here (Developer Tools -> Public Key)',
+        ),
+        'prodId' => array(
+            'FriendlyName' => 'One Time Product ID',
+            'Type' => 'text',
+            'Size' => '25',
+            'Default' => '',
+            'Description' => 'If you are selling one time products, put a Paddle Product ID here for WHMCS to use',
+        ),
+        'taxInclusive' => array(
+            'FriendlyName' => 'Paddle account VAT Settings',
+            'Type' => 'yesno',
+            'Size' => '255',
+            'Default' => '',
+            'Description' => 'Checkout -> Sales Tax Settings -> Include in price ticked?',
+        ),
+        'logoUrl' => array(
+            'FriendlyName' => 'Your Logo URL',
+            'Type' => 'text',
+            'Size' => '255',
+            'Default' => '',
+            'Description' => 'Enter a link to your logo for the Paddle checkout',
+        ),
+        'sandbox'      => [
+            'FriendlyName' => 'Sandbox',
+            'Type'         => 'yesno',
+            'Description'  => 'Tick to enable sandbox mode',
+        ],
+        'webhookUrl'      => [
+            'FriendlyName' => 'Webhook URL',
+            'Description'  => paddle_get_url($systemUrl) . '/modules/gateways/callback/paddle.php',
+        ],
+    );
+}
+
+
+function paddle_link($params)
+{
+
+    $data = paddle_payment_url($params);
+    if (!empty($data->success)) {
+        return '<form action="' . $data->response->url . ' " method="GET">
+        <input class="btn btn-primary" type="submit" value="' . $params['langpaynow'] . '" />
+        </form>';
+    }
+
+    return $data->error->message;
+}
+
+function paddle_payment_url($params)
+{
+    $invoiceId = $params['invoiceid'];
+    $systemUrl = $params['systemurl'];
+
+    $data = array();
+    $data['vendor_id'] = $params['accountID'];
+    $data['vendor_auth_code'] = $params['secretKey'];
+    $data['customer_email'] = $params['clientdetails']['email'];
+    $data['product_id'] = $params["prodId"];
+    $data['title'] = $params["description"];
+    $data['image_url'] = $params["logoUrl"];
+    $data['return_url'] = paddle_get_url($systemUrl) . '/viewinvoice.php?id=' . $invoiceId;
+    $data['marketing_consent'] = $params["marketing_emails_opt_in"];
+    $data['customer_country'] = $params['clientdetails']['country'];
+    $data['customer_postcode'] = $params['clientdetails']['postcode'];
+    $data['passthrough'] = $params['invoiceid'];
+    $data['expires'] = date('Y-m-d', strtotime("+1 days"));
+    $data['prices'] = [
+        'USD' . ":" . $params['amount']
+    ];
+
+    $url = 'https://vendors.paddle.com/api/2.0/product/generate_pay_link';
+    if (!empty($params['sandbox'])) {
+        $url = 'https://sandbox-vendors.paddle.com/api/2.0/product/generate_pay_link';
+    }
+
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => http_build_query($data),
+        CURLOPT_HTTPHEADER => [
+            "Content-Type: application/x-www-form-urlencoded"
+        ],
+    ]);
+
+    $response = curl_exec($curl);
+    curl_close($curl);
+    $result = json_decode($response);
+    return $result;
+}
+
+function paddle_get_url($input)
+{
+    $input = trim($input);
+    if (!preg_match('#^http(s)?://#', $input)) {
+        $input = 'https://' . $input;
+    }
+    $urlHost = parse_url($input, PHP_URL_HOST);
+    $domain = preg_replace('/^www\./', '', $urlHost);
+    return 'https://' . $domain;
+}
